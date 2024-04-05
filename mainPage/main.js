487d9e0c80cb7959bf5f9737c6a2c100120676c7
ArcLKN <kalidolkn@gmail.com>
2024-04-05 16:42:34 +0200

    beginning saveSessionStorage function

--- mainPage/main.js
+++ mainPage/main.js
@@ -1,20 +1,4 @@
 // Initializing base variables
-// Number of people player has, thus number of people he can use.
-var crewTotal;
-// Distribution of the crew, the total can't be > crewTotal.
-var crewTypes = {
-	scouts = 0,
-	guards = 0,
-	carriers = 0  // Free crew becomes carriers to carry more goods.
-};
-var idleCrew; // Number of remaining people to distribute.
-var morale;
-var money;
-var authority;
-var water;
-var numberOfTurns;
-var famineTurns;
-var inventory = [];
 var currentPorterage; // Capacity of the caravan to carry goods. It is influenced by the number of animals such as camels and the number of carriers.
 var neededPorterage; // Weight of all the items transported by the caravan.
 // Each unit of food and water is equal to one weight unit.
--- starterPage/starterScript.js
+++ starterPage/starterScript.js
@@ -1,6 +1,31 @@
+function save(varName, value) {
+	console.log(typeof value);
+	if (typeof value == "number") {value = String(value);}
+	else if (typeof value == "object") {value = JSON.stringify(value);}
+	else if (typeof value == "boolean") {value = String(value);}
+
+	sessionStorage.setItem(String(varName), value);
+}
+
+// Define starting values.
+function saveSessionStorage () {
+	// Number of people player has, thus number of people he can use.
+	save("crewTotal", 10);
+	// Distribution of the crew, the total can't be > crewTotal.
+	save("crewTypes", {scouts = 0,guards = 0,carriers = 0}) // Free crew can become carriers to carry more goods.
+	save("idleCrew", 10); // Number of remaining people to distribute.
+	save("morale", 10);
+	save("money", 10);
+	save("authority", 10);
+	save("water", 10);
+	save("numberOfTurns", 0);
+	save("famineTurns", 0);
+	save("inventory", []);
+	save("playerLocation", 10);
+}
+
 function goPlay() {
 	window.location.href = "../cinematicPage/cinematic.html";
 }
 
 document.getElementById("play").onclick = goPlay;
-sessionStorage.setItem('testVar', 'Raphael');

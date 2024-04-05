7cc524666b76520e90a1a0e0238223cd7cbbe816
ArcLKN <kalidolkn@gmail.com>
2024-04-05 17:58:41 +0200

    Solved error coming from dict syntax

--- starterPage/starterScript.js
+++ starterPage/starterScript.js
@@ -12,7 +12,7 @@ function saveSessionStorage () {
 	// Number of people player has, thus number of people he can use.
 	save("crewTotal", 10);
 	// Distribution of the crew, the total can't be > crewTotal.
-	save("crewTypes", {scouts = 0,guards = 0,carriers = 0}) // Free crew can become carriers to carry more goods.
+	save("crewTypes", {scouts: 0,guards: 0,carriers: 0}); // Free crew can become carriers to carry more goods.
 	save("idleCrew", 10); // Number of remaining people to distribute.
 	save("morale", 10);
 	save("money", 10);
@@ -20,8 +20,9 @@ function saveSessionStorage () {
 	save("water", 10);
 	save("numberOfTurns", 0);
 	save("famineTurns", 0);
-	save("inventory", []);
+	save("inventory", [{name: "food", quantity: 0}]);
 	save("playerLocation", 10);
+	save("gatherer", {water: 0,food: 0,morale: 0})
 }
 
 function goPlay() {

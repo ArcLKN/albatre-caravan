08e838fd823d2f7c50ffb60f36a6d018915f8292
Betelgeuse <hitsujodev@gmail.com>
2024-04-10 00:03:28 +0200

    role attribution update avec raphou mon bb

--- mainPage/AttributionPage/roleAttribution.js
+++ mainPage/AttributionPage/roleAttribution.js
@@ -1,3 +1,60 @@
+// ---------- START MANDATORY VARIABLE FUNCTIONS ----------
+
+// The fonction used to save the files in the user session using sessionStorage.setItem();
+function save(varName, value) {
+  if (typeof value == "number") {
+    value = String(value);
+  } else if (typeof value == "object") {
+    value = JSON.stringify(value);
+  } else if (typeof value == "boolean") {
+    value = String(value);
+  } else {
+    value = String(value);
+  }
+
+  sessionStorage.setItem(String(varName), value);
+}
+
+// Used to get all the new values from the sessionStorage.
+function loadSessionStorage() {
+  crewTotal = parseInt(sessionStorage.getItem("crewTotal"));
+  crewTypes = JSON.parse(sessionStorage.getItem("crewTypes"));
+  idleCrew = sessionStorage.getItem("idleCrew");
+  morale = sessionStorage.getItem("morale");
+  money = sessionStorage.getItem("money");
+  authority = sessionStorage.getItem("authority");
+  water = sessionStorage.getItem("water");
+  numberOfTurns = sessionStorage.getItem("numberOfTurns");
+  famineTurns = parseInt(sessionStorage.getItem("famineTurns"));
+  inventory = JSON.parse(sessionStorage.getItem("inventory"));
+  playerLocation = JSON.parse(sessionStorage.getItem("playerLocation"));
+  gatherer = JSON.parse(sessionStorage.getItem("gatherer"));
+}
+
+// All the variables we want to save and share through every JS files.
+function saveSessionStorage() {
+  // Number of people player has, thus number of people he can use.
+  save("crewTotal", crewTotal);
+  // Distribution of the crew, the total can't be > crewTotal.
+  save("crewTypes", crewTypes); // Free crew can become carriers to carry more goods.
+  save("idleCrew", idleCrew); // Number of remaining people to distribute.
+  save("morale", morale);
+  save("money", money);
+  save("authority", authority);
+  save("water", water);
+  save("numberOfTurns", numberOfTurns);
+  save("famineTurns", famineTurns);
+  save("inventory", inventory);
+  save("playerLocation", playerLocation);
+  save("gatherer", gatherer);
+}
+
+// ---------- END MANDATORY VARIABLE FUNCTIONS ----------
+
+loadSessionStorage();
+
+// ---------- START CREW AND INVENTORY VARIABLES ----------
+
 //Crew related variables
 let crew = [
   {
@@ -5,50 +62,68 @@ let crew = [
     lastName: "Andriamahandry",
     Age: 18,
     Sex: "Female",
+    role: ((scout = false), (guard = false), (carrier = false)),
   },
   {
     name: "Neyla",
     lastName: "Mazouz",
     Age: 19,
     Sex: "Female",
+    role: ((scout = false), (guard = false), (carrier = false)),
   },
   {
     name: "Ethan",
     lastName: "Gandiboyina",
     Age: 19,
     Sex: "Male",
+    role: ((scout = false), (guard = false), (carrier = false)),
   },
   {
     name: "Raphael",
-    lastName: "Greiner",
+    lastName: "Grosnoir",
     Age: 19,
     Sex: "Male",
+    role: ((scout = false), (guard = false), (carrier = false)),
   },
 ];
-let crewTypes = {
-  scouts: 0,
-  guards: 0,
-  carriers: 0, // Free crew becomes carriers to carry more goods.
-};
-let crewTotal = crew.length;
-let idleCrew; // Number of remaining people to distribute.
+
 let crewList = document.getElementById("crewMembers");
 let displayCrewTotal = document.getElementById("crew-total");
-
-//Inventory variable
-let inventory = [];
 let displayHorseTotal = document.getElementById("horse-total");
 let displayWeaponTotal = document.getElementById("weapon-total");
+let scoutAttribution = document.getElementById("roleScout");
+let guardAttribution = document.getElementById("roleGuard");
+let carrierAttribution = document.getElementById("roleCarrier");
 
-//Display the total of crew members
-displayCrewTotal.innerHTML = `Crew: ${crewTotal}` + " | ";
+// ---------- END CREW AND INVENTORY VARIABLES ----------
 
-// !!! METTRE LES VALEURS !!!
-displayHorseTotal.innerHTML = " " + `Horses: ` + " | ";
-displayWeaponTotal.innerHTML = " " + `Weapons: `;
+// ---------- START FUNCTIONS INITIALIZATION ----------
+
+//Display the total of crew members (METTRE LES VALEURS !!)
+function displayResources() {
+  displayCrewTotal.innerHTML = `Crew: ${crewTotal}` + " | ";
+  displayHorseTotal.innerHTML = " " + `Horses: ` + " | ";
+  displayWeaponTotal.innerHTML = " " + `Weapons: `;
+}
+
+scoutAttribution.addEventListener("click", function () {
+  createMemberAndAssign("scout");
+});
+guardAttribution.addEventListener("click", function () {
+  createMemberAndAssign("guard");
+});
+
+function scoutCheck(idInput) {
+  if (document.getElementById(idInput).checked == true) {
+    console.log("trop belle bb" + idInput);
+  }
+}
+function guardCheck(idInput) {
+  console.log("trop belle bb" + idInput);
+}
 
 //Function that create a member component (his image, his informations, his role...)
-function createMember() {
+function createMemberAndAssign(menu = "scout") {
   for (let eachPerson in crew) {
     let person = document.createElement("div");
     let personAbout = document.createElement("div");
@@ -56,18 +131,29 @@ function createMember() {
     let personImage = document.createElement("img");
     let personFullName = document.createElement("span");
     let attributionCheck = document.createElement("input");
-
+    if (menu == "scout") {
+      attributionCheck.addEventListener("click", function () {
+        scoutCheck(this.id);
+      });
+    } else if (menu == "guard") {
+      attributionCheck.addEventListener("click", function () {
+        guardCheck(this.id);
+      });
+    }
     person.classList.add("member");
+    attributionCheck.setAttribute(
+      "id",
+      "personne" + Math.floor(Math.random() * 100000)
+    );
     personAbout.classList.add("member-about");
     attributionContainer.classList.add("checkbox");
-    personImage.src = "personImage-" + eachPerson + ".jpg";
+    personImage.src = "/Images/personImage-" + eachPerson + ".jpg";
     personFullName.classList.add("description");
     personFullName.innerHTML =
       crew[eachPerson]["name"] + " " + crew[eachPerson]["lastName"];
     attributionCheck.type = "checkbox";
     attributionCheck.name = "check";
     attributionCheck.classList.add("check");
-
     person.appendChild(personAbout);
     person.appendChild(attributionContainer);
     personAbout.appendChild(personImage);
@@ -78,7 +164,11 @@ function createMember() {
   }
 }
 
-function roleAttribution() {}
+// ---------- END FUNCTIONS INITIALIZATION ----------
 
-createMember();
-roleAttribution();
+// ---------- START FUNCTIONS CALLING ----------
+
+displayResources();
+createMemberAndAssign();
+
+// ---------- END FUNCTIONS CALLING ----------
--- starterPage/starterScript.js
+++ starterPage/starterScript.js
@@ -1,47 +1,50 @@
 function save(varName, value) {
-	console.log(typeof value);
-	if (typeof value == "number") {value = String(value);}
-	else if (typeof value == "object") {value = JSON.stringify(value);}
-	else if (typeof value == "boolean") {value = String(value);}
+  console.log(typeof value);
+  if (typeof value == "number") {
+    value = String(value);
+  } else if (typeof value == "object") {
+    value = JSON.stringify(value);
+  } else if (typeof value == "boolean") {
+    value = String(value);
+  }
 
-	sessionStorage.setItem(String(varName), value);
+  sessionStorage.setItem(String(varName), value);
 }
 
 // Define starting values.
-function initSessionStorage () {
-	// Number of people player has, thus number of people he can use.
-	save("crewTotal", 10);
-	// Distribution of the crew, the total can't be > crewTotal.
-	save("crewTypes", {scouts: 0,guards: 0,carriers: 0}); // Free crew can become carriers to carry more goods.
-	save("idleCrew", 10); // Number of remaining people to distribute.
-	save("morale", 10);
-	save("money", 10);
-	save("authority", 10);
-	save("water", 10);
-	save("numberOfTurns", 0);
-	save("famineTurns", 0);
-	save("inventory", [{name: "food", quantity: 0}]);
-	save("playerLocation", {
-		type: "desert",
-		name: "Desert",
-		isTradable: false,
-		description: "Bunch of sand.",
-		gatheringValues: {
-		},
-		possibleEvents: [],
-		possibleDestinations: [
-			{type: "village", luck: 0.1}, 
-			{type: "nil_shore", luck: 1}, 
-			{type: "desert", luck: 1}, 
-			{type: "oasis", luck: 0.1}, 
-			{type: "desert_city", luck: 0.1}
-			]
-	});
-	save("gatherer", {water: 0,food: 0,morale: 0})
+function initSessionStorage() {
+  // Number of people player has, thus number of people he can use.
+  save("crewTotal", 10);
+  // Distribution of the crew, the total can't be > crewTotal.
+  save("crewTypes", { scouts: 0, guards: 0, carriers: 0 }); // Free crew can become carriers to carry more goods.
+  save("idleCrew", 10); // Number of remaining people to distribute.
+  save("morale", 10);
+  save("money", 10);
+  save("authority", 10);
+  save("water", 10);
+  save("numberOfTurns", 0);
+  save("famineTurns", 0);
+  save("inventory", [{ name: "food", quantity: 0 }]);
+  save("playerLocation", {
+    type: "desert",
+    name: "Desert",
+    isTradable: false,
+    description: "Bunch of sand.",
+    gatheringValues: {},
+    possibleEvents: [],
+    possibleDestinations: [
+      { type: "village", luck: 0.1 },
+      { type: "nil_shore", luck: 1 },
+      { type: "desert", luck: 1 },
+      { type: "oasis", luck: 0.1 },
+      { type: "desert_city", luck: 0.1 },
+    ],
+  });
+  save("gatherer", { water: 0, food: 0, morale: 0 });
 }
 
 function goPlay() {
-	window.location.href = "../cinematicPage/cinematic.html";
+  window.location.href = "../cinematicPage/cinematic.html";
 }
 
 document.getElementById("play").onclick = goPlay;
